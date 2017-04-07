f341e7a080ca7102539551cf3e97b66bda7af1c7
lichaomin <lichaomin>
2017-04-07 15:04:21 +0800

    接口

---
+++ src/main/java/com/thinkgem/jeesite/common/utils/validate/impl/Amount.java
@@ -0,0 +1,15 @@
+package com.thinkgem.jeesite.common.utils.validate.impl;
+
+import com.thinkgem.jeesite.common.utils.validate.IValidator;
+
+public class Amount implements IValidator{
+
+	private String regex = "";
+	
+	@Override
+	public void test() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
